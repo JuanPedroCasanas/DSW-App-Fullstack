010d69975ea7fc7315b9b3d8c02a8d695c06a864
masofiaochoa <masofiaochoa@gmail.com>
2025-07-26 17:50:43 -0300

    agregado un .bat para correr ambos (be y fe) a la vez

---
+++ run_project.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Iniciando frontend en localhost:3000...
+start cmd /k "cd frontend && npm run dev"
+
+echo Iniciando backend en localhost:2000...
+start cmd /k "cd backend && npm start"
+
+echo Ambos servidores han sido iniciados.
+pause
