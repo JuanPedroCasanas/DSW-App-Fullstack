15aec2fadd1e380ab69f2b7b35dc7484dc15b087
NairP99 <109255058+NairP99@users.noreply.github.com>
2025-10-23 16:17:04 -0300

    Cositas para el lsitado

--- frontend/src/pages/ProfessionalHealthInsurances/ProfessionalHealthInsurances.tsx
+++ frontend/src/pages/ProfessionalHealthInsurances/ProfessionalHealthInsurances.tsx
@@ -1,5 +1,77 @@
+import { useState, useEffect } from 'react';
+import './ProfessionalHelathInsurance.css';
+
+type Professional = {
+  id: string;
+  firstName: string;
+  lastName: string;
+  occupation: Occupation; // ID de especialidad (string por ahora)
+  telephone?: string;
+  isActive: boolean;
+  healthInsurances: HealthInsurance
+};
+type HealthInsurance = {
+  id:string;
+  name:string;
+}
+
+async function handleResponse(res: Response): Promise<{ message: string; type: "success" | "error" }> {
+  const resJson = await res.json().catch(() => ({}));
+
+  if (res.ok) {
+    const successMessage = `${resJson.message} Id: ${resJson.professional?.id}, Apellido y nombre: ${resJson.professional?.lastName} ${resJson.professional?.firstName}`;
+    return { message: successMessage, type: "success" };
+  } else {
+    if (res.status === 500 || res.status === 400) {
+      return { message: resJson.message ?? "Error interno del servidor", type: "error" };
+    } else {
+      const errorMessage = `Error: ${resJson.error} Codigo: ${resJson.code} ${resJson.message}`
+      return { message: errorMessage.trim(), type: "error" };
+    }
+  }
+}
+
+
 export default function ProfessionalHealthInsurances() {
+  const [filtros, setFiltros] = useState({
+    paciente: '',
+    obraSocial: '',
+    fecha: '',
+  });
+  const [list, setList] = useState<Professional[]>([]);
+  const [toast, setToast] = useState<{ message: string; type: "success" | "error" } | null>(null);
+  
+  useEffect(() => {(async () => {
+   
+         const res = await fetch("http://localhost:2000/Professional/getProfessionalsByHealthInsurance");
+  
+        if (!res.ok){
+          const toastData = await handleResponse(res);
+          setToast(toastData);
+        } else {
+          const data: Professional[] = await res.json();
+          const processedData = data.map(professional => ({
+          id: professional.id,
+          nombre: professional.firstName,
+          apellido: professional.lastName,
+          ocupacion: professional.occupation,
+          healthInsurances: professional.healthInsurances
+        }));
+        setList(processedData);
+
+        }
+  
+     })()
+   }, []); 
+  
+  
+
+  
+  
+} 
   return (
     <h1>Listado de obras sociales admitidas por el profesional</h1>
+    
+    
   );
 }
